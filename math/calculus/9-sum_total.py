dfb0c89d847813e1dcd4b862f83010841338e226
aadedigbae <a.adedigba@alustudent.com>
2024-10-05 20:51:24 +0200

    file 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,16 +4,14 @@ the summation game"""
 
 
 def summation_i_squared(n):
-    """ A handy function to sum 
+    """ A handy function to sum
     up i ** 2 for all i's up to n """
 
     # If n is 1, return 1 since 1^2 = 1
     if n == 1:
         return 1
-    
     # If n is less than 1, return None to indicate an invalid input
     if n < 1:
         return None
-    
     result = (n*(n+1)*(2*n+1))//6
     return result
