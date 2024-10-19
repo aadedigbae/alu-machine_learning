08805867c9f027cdd3b7c8c2a4d2655c7f0c37cb
aadedigbae <a.adedigba@alustudent.com>
2024-10-19 19:38:24 +0200

    last

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -4,11 +4,13 @@ import numpy as np
 
 
 def mean_cov(X):
+    """Let's get the mean and covariance of a data set"""
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         raise TypeError('X must be a 2D numpy.ndarray')
     n, d = X.shape
     if n < 2:
         raise ValueError("X must contain multiple data points")
+    
     m = np.mean(X, axis=0, keepdims=True)  # Mean of the data points
     C = np.matmul((X - m).T, (X - m)) / (n - 1)  # Covariance matrix
     return m, C
--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -4,10 +4,12 @@ import numpy as np
 
 
 def correlation(C):
+    """let's get the correlation matrix"""
     if not isinstance(C, np.ndarray):
         raise TypeError('C must be a numpy.ndarray')
     if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError('C must be a 2D square matrix')
+    
     var_values = np.diag(np.sqrt(np.diag(C)))
     cor = var_values @ C @ var_values  # Correlation matrix
     return cor
